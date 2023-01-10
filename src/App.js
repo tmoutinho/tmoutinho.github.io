70b122a5f6ed818db8a717236da1f67c1d7175ef
Tiago Moutinho <tiagoagm@gmail.com>
2023-01-10 17:52:22 +0000

    print distinct id and speed in browser

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./apelaser.png";
 import "./App.css";
 
 // Web3 Wallet Connect Shit
-import Web3 from "web3";
+// import Web3 from "web3";
 
 // New SDK from NPM
 import frame from "@conductiveai/conductive-sdk";
@@ -11,19 +11,19 @@ import React from "react";
 
 // Wallet connect function
 // const web3 = new Web3('ws://localhost:3001');
-const web3 = new Web3("wss://ericlsung.com/AstroChimp");
-async function connect() {
-  if (window.ethereum) {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    window.web3 = new Web3(window.ethereum);
-    const account = web3.eth.accounts;
-    //Get the current MetaMask selected/active wallet
-    const walletAddress = account.givenProvider.selectedAddress;
-    console.log(`Wallet: ${walletAddress}`);
-  } else {
-    console.log("No wallet");
-  }
-}
+// const web3 = new Web3("wss://ericlsung.com/AstroChimp");
+// async function connect() {
+//   if (window.ethereum) {
+//     await window.ethereum.request({ method: "eth_requestAccounts" });
+//     window.web3 = new Web3(window.ethereum);
+//     const account = web3.eth.accounts;
+//     //Get the current MetaMask selected/active wallet
+//     const walletAddress = account.givenProvider.selectedAddress;
+//     console.log(`Wallet: ${walletAddress}`);
+//   } else {
+//     console.log("No wallet");
+//   }
+// }
 
 function App() {
   React.useEffect(
@@ -58,13 +58,16 @@ function App() {
           </li>
         </ul>
 
+        <div id="dictinctId">Computing...</div>
+        <div id="speedId">Computing speed...</div>
+
         {/* <input className="wconnect" type="button" value="Connect Wallet" onClick={connect}/> */}
 
-        <div className="wrapper">
+        {/* <div className="wrapper">
           <a className="cta" href="#" onClick={connect}>
             <span>Connect Wallet</span>
           </a>
-        </div>
+        </div> */}
       </header>
     </div>
   );

1747b45ca87be59758b526f9f18b14ab0af10f9b
Tiago Moutinho <tiagoagm@gmail.com>
2023-02-15 11:05:22 +0000

    Update new version and UI

--- src/App.js
+++ src/App.js
@@ -1,92 +1,53 @@
 import logo from "./apelaser.png";
-import "./App.css";
-
-// Web3 Wallet Connect Shit
-// import Web3 from "web3";
-
-// New SDK from NPM
 import frame from "@conductiveai/conductive-sdk";
-import React from "react";
-// import { BlockHeader, Block } from 'web3-eth' // ex. package types
-
-// Wallet connect function
-// const web3 = new Web3('ws://localhost:3001');
-// const web3 = new Web3("wss://ericlsung.com/AstroChimp");
-// async function connect() {
-//   if (window.ethereum) {
-//     await window.ethereum.request({ method: "eth_requestAccounts" });
-//     window.web3 = new Web3(window.ethereum);
-//     const account = web3.eth.accounts;
-//     //Get the current MetaMask selected/active wallet
-//     const walletAddress = account.givenProvider.selectedAddress;
-//     console.log(`Wallet: ${walletAddress}`);
-//   } else {
-//     console.log("No wallet");
-//   }
-// }
+import React, { useEffect } from "react";
+import { WalletConnect } from "./wallet-connect";
 
 function App() {
-  React.useEffect(() => {
-    frame.init("pIoFHWz77KQIMIAPt7lTyMc_cSrcB2MmwDsaQTTaEn4", {
+  useEffect(() => {
+    frame.init("16OG2oVu4GYg8CjfOQKHl_O7htLut7avxzjNbfLWyuc", {
       api_host: "https://analytics.dev-conductive.ai",
       debug: true,
+      loaded: (frame) => {
+        window.frame = frame;
+      },
     });
-
-    setTimeout(() => {
-      if (window.frameUtils.distinct_id) {
-        document.getElementById("fingerprintId").innerHTML =
-          window.frameUtils.distinct_id;
-      }
-
-      if (window.frameUtils.old_distinct_id) {
-        document.getElementById("oldDistinctId").innerHTML =
-          window.frameUtils.old_distinct_id;
-      }
-    }, 2000);
-
-    // return () => {
-    //   timeout.clearTimeout();
-    // };
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img id="NFT" src={logo} className="App-logo" alt="logo" />
+    <div className="container my-4 mx-auto space-y-4">
+      <h1 className="text-3xl font-bold">SDK testing env</h1>
 
-        <p id="goham">
-          {/* Testing NPM install of SDK 
-          <br/>Pointed to QA analytics. 
-          <br/>SDK-test project. */}
-          New NPM Install New SDK
-        </p>
+      <img
+        id="NFT"
+        src={logo}
+        className="h-32 w-32 rounded-full ring-2 ring-black"
+        alt="logo"
+      />
 
-        <ul>
-          <li>
-            <a href="https://ape.to/zwcjk">NPM Prod Campaign link </a>
-          </li>
-          <li>
-            <a href="https://discord.ape.to/J3BPT9KsRJ">
-              NPM Prod Discord Join{" "}
-            </a>
-          </li>
-        </ul>
+      <div className="flex">
+        <WalletConnect />
+      </div>
 
-        <div>
-          Fingerprint ID: <span id="fingerprintId">Loading...</span>
-        </div>
-        <div>
-          Distinct ID: <span id="oldDistinctId">Loading...</span>
-        </div>
-
-        {/* <input className="wconnect" type="button" value="Connect Wallet" onClick={connect}/> */}
-
-        {/* <div className="wrapper">
-          <a className="cta" href="#" onClick={connect}>
-            <span>Connect Wallet</span>
+      <div className="">
+        <div className="flex mb-4">
+          <a
+            href="https://ape.to/zwcjk"
+            className="rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800 bg-slate-100 text-slate-900 hover:bg-slate-200 h-10 py-2 px-4"
+          >
+            NPM Prod Campaign link
           </a>
-        </div> */}
-      </header>
+        </div>
+
+        <div className="flex">
+          <a
+            href="https://discord.ape.to/J3BPT9KsRJ"
+            className="rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800 bg-slate-100 text-slate-900 hover:bg-slate-200 h-10 py-2 px-4"
+          >
+            NPM Prod Discord Join
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/wallet-connect.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { ethers } from "ethers";
+
+export function WalletConnect() {
+  let [userAccount, setUserAccount] = useState({
+    isConnect: false,
+    Account: "",
+  });
+
+  let isItConnect = async () => {
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let accounts = await provider.send("eth_requestAccounts", []);
+
+    if (accounts.length > 0) {
+      return {
+        status: true,
+        userAddress: accounts[0],
+      };
+    } else {
+      return {
+        status: false,
+        userAddress: "",
+      };
+    }
+  };
+
+  let connect = async () => {
+    let Status = await isItConnect();
+    localStorage.setItem("isConnected", Status.status);
+    setUserAccount((prev) => {
+      return { ...prev, Account: Status.userAddress };
+    });
+  };
+
+  window.ethereum.on("accountsChanged", async () => {
+    localStorage.removeItem("isConnected");
+    setUserAccount((prev) => {
+      return { ...prev, Account: "" };
+    });
+    // connect();
+  });
+
+  // useEffect(() => {
+  //   let status = localStorage.getItem("isConnected");
+  //   if (status) {
+  //     connect();
+  //   }
+  //   if (status === null) {
+  //     if (window.ethereum.selectedAddress === null) {
+  //       //console.log("welcome User!");
+  //     } else {
+  //       connect();
+  //     }
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  return (
+    <>
+      {userAccount.Account === "" && (
+        <button
+          onClick={connect}
+          className="rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800 bg-slate-900 text-white hover:bg-slate-700 h-10 py-2 px-4"
+        >
+          Connect Wallet
+        </button>
+      )}
+      {userAccount.Account !== "" && (
+        <div className="p-2 border border-green-500">
+          <p className="text-xs">Wallet address</p>
+          <p className="text-base font-semibold">{userAccount.Account}</p>
+        </div>
+      )}
+    </>
+  );
+}
